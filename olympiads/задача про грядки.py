3dfdcee68cb0442e69ae0b46584df8d86d4d7bcf
breaststr0ke <colo.il@yandex.ru>
2023-09-23 21:17:44 +0600

    Update задача про грядки.py

--- olympiads/задача про грядки.py
+++ olympiads/задача про грядки.py
@@ -1,8 +1,14 @@
 # формула
-k = [1,2,3,20]
+'''k = [1,2,3,20]
 l = 7
 n = 5
 m = 10
 for i in range(len(k)):
     S = (2 * k[i] * l) + (k[i] * (k[i]+1) * m) + (2 * n * k[i])
-    print(S)
+    print(S)'''
+
+m = 10
+n = 5
+l = 7
+k = [1,2,3,20]
+for k in range()
