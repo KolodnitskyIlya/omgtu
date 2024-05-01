b2a980043a8f0edfe4345dc9ed2df3e92ea73466
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-05-01 21:59:19 +0600

    Wave's algorithm

---
+++ discrete mathematics/Wave's algorithm.py
@@ -0,0 +1,65 @@
+import random
+
+matrix = [
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0]
+]
+start = [3, 3]
+finish = [0, 6]
+
+for i in range(0, len(matrix)):
+    for j in range(0, len(matrix[i])):
+        rand = random.random()
+        if rand > 0.7:
+            if not (i == start[0] and j == start[1]):
+                if not (i == finish[0] and j == finish[1]):
+                    matrix[i][j] = -1
+
+def output():
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            print(matrix[i][j], end='\t')
+        print(' ')
+    print(' ')
+    
+output()
+
+matrix[start[0]][start[1]] = 1
+
+output()
+
+check = True
+
+while check:
+    check = False
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            if matrix[i][j] > 0:
+                for y in range(i-1, i + 2):
+                    for x in range(j - 1, j + 2):
+                        if 0 <= y <= 7 and 0 <= x <= 6:
+                                
+                            if matrix[y][x] == 0:
+                                matrix[y][x] = matrix[i][j]+1
+                                check = True
+
+                            if matrix[y][x] >= 0:
+                                if matrix[i][j] + 1 < matrix[y][x]:
+                                    matrix[x][y] = matrix[i][j] + 1
+                                    check = True
+
+for i in range(0, len(matrix)):
+    for j in range(0, len(matrix[i])):
+        print(matrix[i][j], end='\t')
+    print(' ')
+if(matrix[finish[0]][finish[1]])!= 0:
+    print(matrix[finish[0]][finish[1]])
+else:
+    print('impossible')
+    
