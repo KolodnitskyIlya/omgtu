8873a99c2fc5d33068c056259d475a188f137ad3
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-05-09 02:00:15 +0600

    Branch and bounds

--- discrete mathematics/Branch and bounds.py
+++ discrete mathematics/Branch and bounds.py
@@ -44,16 +44,6 @@ def zapret_way(): # to do
             matrix[way[1][1]][way[1][0]] = inf
             print(way[1][1], way[1][0])
 
-
-# сумма минимальных по столбцам и строкам - минимальная граница
-
-# нахождение степеней нулей (минимальное значение в строке и столбце за исключением этого элемента)
-
-# куда берем ноль с максимальной степенью - мин граница, в другую сторону прибавляем степень нуля к мин границе
-
-# уничтожаем строку и столбец и добавляем ячейку бесконечности
-# на итерации: минимумы по строке, уменьшили, минимумы по слобцам, уменьшили, сложили, посчитали оценку, посчитали степени нуля, выбрали наибольшую
-
 inf = float('inf')
 matrix = [
     [inf, 41, 80, 23, 32],
@@ -72,7 +62,6 @@ while len(stolbci) + len(stroki) != 4:
     alpha = [min(x) for x in matrix]
     print(alpha)
 
-    # - alpha
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             if matrix[i][j] != inf:
@@ -80,14 +69,12 @@ while len(stolbci) + len(stroki) != 4:
     print("after alpha")
     output()
 
-    # beta count
     beta = [x for x in matrix[0]]
     for i in range(1, len(matrix)):
         for j in range(0, len(matrix[i])):
             beta[j] = min(beta[j], matrix[i][j])
     print(beta)
 
-    # - beta
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
             if matrix[i][j] != inf:
