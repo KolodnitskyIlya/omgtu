cc641db1ba339c49bef7f9fcea090840473715c7
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-05-15 00:17:18 +0600

    linq1

---
+++ algorithmization and programming/The second semester/linq1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = new int[] { 124, 123, 465, 12385, 456, 123, 49, 5644, 3765 };
+            elements
+                .Where(e => e % 10 % 3 == 0)
+                .ToList()
+                .ForEach(e => Console.Write(e + " "));
+        }
+    }
+}
