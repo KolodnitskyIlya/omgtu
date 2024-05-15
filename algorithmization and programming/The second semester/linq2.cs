7dd3c0a5c87d028ecdff21ae1c78bf1a5c0e07eb
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-05-15 19:50:44 +0600

    linq2

---
+++ algorithmization and programming/The second semester/linq2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int NumberSum(int number)
+        {
+            return number.ToString().ToArray().Aggregate<char, int>(0, (sum, el) => sum += int.Parse(el + ""));
+        }
+
+        static void Main(string[] args)
+        {
+            int[] elements = new int[] { 345, 123, 45, 11, 46, 1253, 439, 5654, 3763 };
+            elements
+                .Where(e => NumberSum(e) % 2 == 0)
+                .ToList()
+                .ForEach(e => Console.Write(e + " "));
+        }
+    }
+}
