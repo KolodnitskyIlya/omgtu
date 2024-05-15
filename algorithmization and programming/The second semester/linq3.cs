2741f640e6615aaa72042b41dd1a11a9ae4cced7
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-05-15 19:53:45 +0600

    linq3

---
+++ algorithmization and programming/The second semester/linq3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> data = new List<string>(new string[] { "1345", "12345678", "123456", "1234567", "12347", "123489", "134", "12378" });
+            var el = from e in data
+                     where e.Length % 2 == 0
+                     select e;
+
+            foreach (var e in el)
+                Console.Write(e + " ");
+
+            Console.WriteLine();
+            int i = 1;
+            while (i < data.Count)
+                data.RemoveAt(i++);
+
+            foreach (var e in el)
+                Console.Write(e + " ");
+
+        }
+    }
+}
