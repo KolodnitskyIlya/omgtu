e74546266978189cf0985b70f88f8d8809040576
Колодницкий Илья <80146551+KolodnitskyIlya@users.noreply.github.com>
2024-05-21 19:15:33 +0600

    Update bank accounts.cs

--- algorithmization and programming/The second semester/bank accounts.cs
+++ algorithmization and programming/The second semester/bank accounts.cs
@@ -1,72 +1,72 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Collections.Generic;
-using Test;
 
-namespace Test;
-
-class Account
+namespace Test
 {
-    public int ID;
-    public string name;
-    public int income;
-    public int expenses;
-    public double tax;
-
-    public Account(int ID, string name, int income, int expenses)
+    class Account
     {
-        this.ID = ID;
-        this.name = name;
-        this.income = income;
-        this.expenses = expenses;
-        tax = income * 0.05;
+        public int ID;
+        public string name;
+        public int income;
+        public int expenses;
+        public double tax;
+
+        public Account(int ID, string name, int income, int expenses)
+        {
+            this.ID = ID;
+            this.name = name;
+            this.income = income;
+            this.expenses = expenses;
+            tax = income * 0.05;
+        }
     }
-}
 
-public static class Program
-{
-    public static void Main()
+    public static class Program
     {
-        List<Account> Accounts = new List<Account>(){
-            new Account(1, "Brandy Abbott", 85545, 50456),
-            new Account(2, "Ashley Powell", 67123, 67123),
-            new Account(3, "Stacy Thompson", 10400, 106400),
-            new Account(4, "Carol Schmidt", 61234, 59321),
-            new Account(5, "Amanda Maldonado", 55654, 1654)
-        };
-
-        Console.WriteLine("Счета с отрицательным балансом:");
-
-        var query = from account in Accounts
-                    where (account.income - account.expenses - account.tax < 0)
-                    select account;
-
-        foreach (Account account in query)
+        public static void Main()
         {
-            Console.WriteLine($" - {account.name}");
+            List<Account> accounts = new List<Account>(){
+                new Account(1, "Brandy Abbott", 85545, 50456),
+                new Account(2, "Ashley Powell", 67123, 67123),
+                new Account(3, "Stacy Thompson", 10400, 106400),
+                new Account(4, "Carol Schmidt", 61234, 59321),
+                new Account(5, "Amanda Maldonado", 55654, 1654)
+            };
+
+            Console.WriteLine("Счета с отрицательным балансом:");
+
+            var query = from account in accounts
+                        where (account.income - account.expenses - account.tax < 0)
+                        select account;
+
+            foreach (Account account in query)
+            {
+                Console.WriteLine($" - {account.name}");
+            }
+            Console.WriteLine();
+
+            var query2 = from account in accounts
+                         where (account.income - account.expenses - account.tax > 0)
+                         select account;
+
+            Console.WriteLine($"Количество счетов с положительным балансом: {query2.Count()} \n");
+
+            double maxIncome = accounts.Max(x => x.income);
+            var query3 = from account in accounts
+                         where account.income == maxIncome
+                         select account;
+
+            Console.WriteLine("Счета с максимальным балансом:");
+
+            foreach (Account account in query3)
+            {
+                Console.WriteLine($" - {account.name}");
+            }
+            Console.WriteLine();
+
+            double totalTaxes = accounts.Sum(x => x.tax);
+            Console.WriteLine($"Общая сумма налогов: {totalTaxes}");
         }
-        Console.WriteLine();
-
-        var query2 = from account in Accounts
-                     where (account.income - account.expenses - account.tax > 0)
-                     select account;
-
-        Console.WriteLine($"Количество счетов с положительным балансом: {query2.Count()} \n");
-
-        var query3 = from account in Accounts
-                     let x = Accounts.Max(x => x.income)
-                     where (account.income == x)
-                     select account;
-
-        Console.WriteLine("Счета с максимальным балансом:");
-
-        foreach (Account account in query3)
-        {
-            Console.WriteLine($" - {account.name}");
-        }
-        Console.WriteLine();
-
-        double total_taxes = Accounts.Select(x => x.tax).ToList().Sum();
-        Console.WriteLine($"Общая сумма налогов: {total_taxes}");
     }
 }
